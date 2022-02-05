61d6c7f922a46f3f2c2e6a6eb6d2926c304ca0d7
Kevin Buck <asllamarguy@me.com>
2022-02-05 18:18:29 -0500

    init commit

---
+++ aboutMe/View/AboutMeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutMeViewController.swift
+//  aboutMe
+//
+//  Created by Kevin Buck on 11/10/21.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
